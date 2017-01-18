66535f5b8dfa3add69d8a41c130c45a0f44e7d08
Marco Strobel <mail@strobel.ch>
2017-01-18 01:41:50 +0100

    fix typo in example

--- example/bootstrap/systemjs.config.js
+++ example/bootstrap/systemjs.config.js
@@ -3,20 +3,20 @@ SystemJS.config({
     "example-bootstrap": "src/"
   },
   "map": {
-    "jquery": "node_modules/jquery/dist",
-    "bootstrap": "node_modules/bootstrap/dist/js"
+    "bootstrap": "node_modules/bootstrap/dist",
+    "jquery": "node_modules/jquery/dist"
   },
   "packages": {
-    "node_modules/jquery/dist": {
-      "main": "jquery.js"
-    },
-    "node_modules/bootstrap/dist/js": {
-      "main": "bootstrap.js",
+    "node_modules/bootstrap/dist": {
+      "main": "js/bootstrap.js",
       "format": "global",
       "deps": [
         "jquery"
       ],
       "exports": "$"
+    },
+    "node_modules/jquery/dist": {
+      "main": "jquery.js"
     },
     "example-bootstrap": {
       "main": "index.js"
