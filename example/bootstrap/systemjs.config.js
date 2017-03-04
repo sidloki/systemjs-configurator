c1abbd8387e85b39c4f84df1ff2f8abac52c2fb8
Marco Strobel <mail@strobel.ch>
2017-03-04 20:39:41 +0100

    update bootstrap example

--- example/bootstrap/systemjs.config.js
+++ example/bootstrap/systemjs.config.js
@@ -1,12 +1,15 @@
 SystemJS.config({
   "paths": {
-    "example-bootstrap": "src/"
+    "example-bootstrap/": "src/"
   },
   "map": {
     "bootstrap": "node_modules/bootstrap/dist",
     "jquery": "node_modules/jquery/dist"
   },
   "packages": {
+    "example-bootstrap": {
+      "main": "index.js"
+    },
     "node_modules/bootstrap/dist": {
       "main": "js/bootstrap.js",
       "format": "global",
@@ -17,9 +20,6 @@ SystemJS.config({
     },
     "node_modules/jquery/dist": {
       "main": "jquery.js"
-    },
-    "example-bootstrap": {
-      "main": "index.js"
     }
   }
 });
